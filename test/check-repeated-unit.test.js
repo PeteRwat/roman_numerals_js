a96aef84d1ec4914403224549cfb19571a7288be
Peter Rwatschew <pete.rwatschew@bbc.co.uk>
2020-11-06 23:26:25 +0000

    add stringContainsRepeatedUnit function

---
+++ test/check-repeated-unit.test.js
@@ -0,0 +1,34 @@
+const stringContainsRepeatedUnit = require('../src/check-repeated-unit')
+
+test("returns true for string containing IIII", () => {
+    const inputString = "IIIIV"
+    
+    const returnedVal = stringContainsRepeatedUnit(inputString)
+
+    expect(returnedVal).toBe(true)
+})
+
+test("returns true for string containing XXXX", () => {
+    const inputString = "IXXXXV"
+    
+    const returnedVal = stringContainsRepeatedUnit(inputString)
+
+    expect(returnedVal).toBe(true)
+})
+
+test("returns true for string containing CCCC", () => {
+    const inputString = "CCCCIV"
+    
+    const returnedVal = stringContainsRepeatedUnit(inputString)
+
+    expect(returnedVal).toBe(true)
+})
+
+test("returns true for string containing MMMM", () => {
+    const inputString = "IMMMMDASV"
+    
+    const returnedVal = stringContainsRepeatedUnit(inputString)
+
+    expect(returnedVal).toBe(true)
+})
+
