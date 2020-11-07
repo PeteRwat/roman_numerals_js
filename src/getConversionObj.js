fc9e9e8acba0c87bc9facd3ba5cc11f7cf5391ba
Peter Rwatschew <pete.rwatschew@bbc.co.uk>
2020-11-07 15:08:07 +0000

    add getconversion fun

---
+++ src/getConversionObj.js
@@ -0,0 +1,7 @@
+const conversions = require('./conversions.json')
+
+const getConversionObj = (numeral) => {
+    return conversions.filter(conversionObj => conversionObj.numeral == numeral)[0]
+}
+
+module.exports = getConversionObj
