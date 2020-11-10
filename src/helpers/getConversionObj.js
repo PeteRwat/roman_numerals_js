567a8cbe8dd83f16d1f943110ef13ddc3568fe77
Peter Rwatschew <pete.rwatschew@bbc.co.uk>
2020-11-10 10:30:44 +0000

    remove getConversionObj fun

--- src/helpers/getConversionObj.js
+++
@@ -1,7 +0,0 @@
-const conversions = require('../conversions.json')
-
-const getConversionObj = (numeral) => {
-    return conversions.filter(conversionObj => conversionObj.numeral == numeral)[0]
-}
-
-module.exports = getConversionObj
